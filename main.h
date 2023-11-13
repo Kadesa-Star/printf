9c8ad1af28feca36635c6886f025a2e277ea25a8
Lextheuri <lextheuri90@gmail.com>
2023-11-13 08:05:30 -0800

    main.h commit

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef "MAIN_H"
+#define "MAIN_H"
+
+int _printf(const char *format, ...);
+void handle_char(int *count, va_list args);
+void handle_string(int *count, va_list args);
+void handle_percent(int *count);
+int _printf(const char *format, ...);
+int _putchar(char c);
+#endif
