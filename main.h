a2da85c8732bdf77d884da03cb5668a43eb638ed
Lextheuri <lextheuri90@gmail.com>
2023-11-14 03:13:16 -0800

    Handle binary file commit

--- main.h
+++ main.h
@@ -5,6 +5,7 @@ int _printf(const char *format, ...);
 void handle_char(int *count, va_list args);
 void handle_string(int *count, va_list args);
 void handle_percent(int *count);
+void handle_binary(int *count, va_list args);
 int _printf(const char *format, ...);
 int _putchar(char c);
 void print_i(va_list args);
---
+++ printf_handle_binary.c
@@ -0,0 +1,64 @@
+#include "main.h"
+/**
+ * handle_binary - Helper function to handle binary format specifier
+ * @count: Pointer to character count
+ * @args: Variable arguments list
+ * Return: NULL
+ */
+void handle_binary(int *count, va_list args);
+void handle_binary(int *count, va_list args)
+{
+unsigned int num = va_arg(args, unsigned int);
+unsigned int mask = 1 << (sizeof(unsigned int) * 8 - 1);
+while (mask > 0)
+{
+putchar((num & mask) ? '1' : '0');
+mask >>= 1;
+(*count)++;
+}
+}
+int _printf(const char *format, ...)
+{
+va_list args;
+va_start(args, format);
+int count = 0;
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+case 'c':
+handle_char(&count, args);
+break;
+case 's':
+handle_string(&count, args);
+break;
+case 'd':
+case 'i':
+handle_int(&count, args);
+break;
+case 'b':
+handle_binary(&count, args);
+break;
+case '%':
+handle_percent(&count);
+break;
+default:
+handle_percent(&count);
+putchar(*format);
+count += 2;
+break;
+}
+}
+else
+{
+putchar(*format);
+count++;
+}
+format++;
+}
+va_end(args);
+return (count);
+}
